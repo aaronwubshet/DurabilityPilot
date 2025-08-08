1d0438aa8af15b00c89e9dc11c951afb605623d0
Aaron Wubshet <13680876+aaronwubshet@users.noreply.github.com>
2025-08-08 12:52:34 -0400

    Initial Commit

---
+++ Durability/DurabilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  DurabilityApp.swift
+//  Durability
+//
+//  Created by Aaron Wubshet on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct DurabilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
